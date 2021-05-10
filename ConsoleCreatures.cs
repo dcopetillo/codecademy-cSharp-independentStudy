cdea4870dce1e674b2596acc8cb3f927ad394433
dcopetillo <46605014+dcopetillo@users.noreply.github.com>
2021-05-10 15:32:26 -0400

    Add files via upload

---
+++ ConsoleCreatures.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      /*This program is used for creating an ASCII ghost art*/
+      Console.WriteLine(" .-.");  
+      Console.WriteLine("(o o)"); 
+      Console.WriteLine("| O |");
+      Console.WriteLine("|   |"); 
+      Console.WriteLine("'~~~'"); 
+      Console.WriteLine("Gabby the Ghost");    Console.WriteLine("\\");  
+
+     }
+  }
+}
